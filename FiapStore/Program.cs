c3aa0b8c136b4240a221519f20db9ba6b162c2e3
Rafael Ferreira Rodrigues <rafael.frodrigues13@gmail.com>
2025-10-13 18:45:36 -0300

    Add files via upload

---
+++ FiapStore/Program.cs
@@ -0,0 +1,41 @@
+using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("ConnectionString"));
+}, ServiceLifetime.Scoped);
+
+/*builder.Services.AddScoped<ILivro, LivroRepository>();
+builder.Services.AddScoped<IPedido, PedidoRepository>();
+builder.Services.AddScoped<IClienteRepository, ClienteRepository()>*/
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
