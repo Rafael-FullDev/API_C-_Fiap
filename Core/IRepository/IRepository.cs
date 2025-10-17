eea2a7c2a3a6d3bfcaeb181ae0cbf58f05e91b3a
Rafael Ferreira Rodrigues <rafael.frodrigues13@gmail.com>
2025-10-17 18:11:00 -0300

    Add files via upload

---
+++ Core/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entity;
+
+namespace Core.IRepository
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+        IList<T> Lista();
+        T ObterPorId(int Id);
+        void Cadastrar(T entidade);
+        void Alterar(T entidade);
+        void Deletar(int Id);
+
+    }
+}
---
+++ Core/Inputs/LivroInput.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Inputs
+{
+    public class LivroInput
+    {
+        public required string Nome { get; set; }
+        public required string Editora { get; set; }
+    }
+}
